2bd786e3e3768cbbb7119f6abb00896123ddfbb4
ErwanHermawan <erwanhermawan858@gmail.com>
2024-04-02 16:07:54 +0700

    upadate banner

--- src/presentation/component/organisms/Banner/index.jsx
+++ src/presentation/component/organisms/Banner/index.jsx
@@ -1,16 +1,19 @@
-const Banner = (props) => {
-	const { data } = props;
+// -- style
+import style from "./style.module.scss";
 
+const Banner = ({ data }) => {
 	return (
-		<div className="banner">
-			<div className="banner__img">
-				<img src={data.image} alt="Banner" className="banner__img__el" />
+		<div className={style.banner}>
+			<div className={style.image}>
+				<img src={data.image} alt={data.title} />
 			</div>
-			<div className="banner__text">
-				<h1 className="banner__title">{data.title}</h1>
-				{data.description !== undefined ? (
-					<p className="banner__desc">{data.description}</p>
-				) : null}
+			<div className="container">
+				<div className={style.text}>
+					<h1 className={style.title}>{data.title}</h1>
+					{data.description !== undefined ? (
+						<p className={style.description}>{data.description}</p>
+					) : null}
+				</div>
 			</div>
 		</div>
 	);
